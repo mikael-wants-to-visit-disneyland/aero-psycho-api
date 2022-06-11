bb060d786e728ad362eb5f6020d56a87027deea6
Test <test@Mikaels-MacBook-Pro.local>
2022-06-11 22:24:37 +0200

    init

---
+++ sqs/receiver.ts
@@ -0,0 +1,15 @@
+import { SQSHandler, SQSMessageAttributes } from 'aws-lambda';
+
+const receiver: SQSHandler = async (event) => {
+  try {
+    for (const record of event.Records) {
+      const messageAttributes: SQSMessageAttributes = record.messageAttributes;
+      console.log(messageAttributes);
+      // Do something
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default receiver;
