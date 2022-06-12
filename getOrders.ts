43e503115790b297bb79a6910dbd9cb9b7b60da5
Test <test@Mikaels-MacBook-Pro.local>
2022-06-12 14:58:13 +0200

    abstractified expression params

--- getOrders.ts
+++ getOrders.ts
@@ -2,18 +2,31 @@
 import { DynamoDB } from "aws-sdk";
 
 module.exports.getOrders = async (event) => {
-  console.log(event.queryStringParameters);
+  console.log(Object.entries(event.queryStringParameters));
+  console.log(
+    Object.entries(event.queryStringParameters)
+      .map(([key, value], _) => `${key} = ${value}`)
+      .join(" AND ")
+  );
+
+  const attributeNames = Object.keys(event.queryStringParameters).map(
+    (name, i) => ({ [`#${i}`]: name })
+  );
+  const attributeValues = Object.values(event.queryStringParameters).map(
+    (value, i) => ({ [`:${i}`]: value })
+  );
+  const filterExpression = attributeNames
+    .map((_, i) => `#${i} = :${i}`)
+    .join(" AND ");
+
+  // make the queryable keys into env vars
 
   // do check and statuscode error return for bad parameter
   const scanParams = {
     TableName: process.env.DYNAMODB_ORDERS_TABLE,
-    FilterExpression: "#sid = :sid",
-    ExpressionAttributeNames: {
-      "#sid": "sellerId",
-    },
-    ExpressionAttributeValues: {
-      ":sid": "guuuuu",
-    },
+    FilterExpression: filterExpression,
+    ExpressionAttributeNames: Object.assign({}, ...attributeNames),
+    ExpressionAttributeValues: Object.assign({}, ...attributeValues),
   };
 
   const dynamoDB = new DynamoDB.DocumentClient();
