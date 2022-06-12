81d57dc446919ea26737962692b4e67ad2ddefd9
Test <test@Mikaels-MacBook-Pro.local>
2022-06-12 15:06:27 +0200

    extracted getFilterExpressionParams

--- getOrders.ts
+++ getOrders.ts
@@ -1,34 +1,35 @@
 "use strict";
 import { DynamoDB } from "aws-sdk";
 
-module.exports.getOrders = async (event) => {
-  console.log(Object.entries(event.queryStringParameters));
-  console.log(
-    Object.entries(event.queryStringParameters)
-      .map(([key, value], _) => `${key} = ${value}`)
-      .join(" AND ")
-  );
-
-  const attributeNames = Object.keys(event.queryStringParameters).map(
-    (name, i) => ({ [`#${i}`]: name })
-  );
-  const attributeValues = Object.values(event.queryStringParameters).map(
-    (value, i) => ({ [`:${i}`]: value })
-  );
+const getFilterExpressionParams: (filterParams: Record<string, string>) => {
+  FilterExpression: string;
+  ExpressionAttributeNames: Record<string, string>;
+  ExpressionAttributeValues: Record<string, string>;
+} = (filterParams) => {
+  const attributeNames = Object.keys(filterParams).map((name, i) => ({
+    [`#${i}`]: name,
+  }));
+  const attributeValues = Object.values(filterParams).map((value, i) => ({
+    [`:${i}`]: value,
+  }));
   const filterExpression = attributeNames
     .map((_, i) => `#${i} = :${i}`)
     .join(" AND ");
+  return {
+    FilterExpression: filterExpression,
+    ExpressionAttributeNames: Object.assign({}, ...attributeNames),
+    ExpressionAttributeValues: Object.assign({}, ...attributeValues),
+  };
+};
 
+module.exports.getOrders = async (event) => {
   // make the queryable keys into env vars
 
   // do check and statuscode error return for bad parameter
   const scanParams = {
     TableName: process.env.DYNAMODB_ORDERS_TABLE,
-    FilterExpression: filterExpression,
-    ExpressionAttributeNames: Object.assign({}, ...attributeNames),
-    ExpressionAttributeValues: Object.assign({}, ...attributeValues),
+    ...getFilterExpressionParams(event.queryStringParameters),
   };
-
   const dynamoDB = new DynamoDB.DocumentClient();
   const result = await dynamoDB.scan(scanParams).promise();
 
