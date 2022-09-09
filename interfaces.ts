de93517625d8c6aa941009240378ff3730c678a9
Mikael Koskela <mikaelkoskela@Mikaels-MBP-2.lan>
2022-09-09 16:12:52 +0100

    removed unnecessary attributes from dynamodb

--- interfaces.ts
+++ interfaces.ts
@@ -4,20 +4,47 @@ import moment from "moment";
 export interface IFlight {
   flightCode: string;
   date: string;
+  estimatedDepartureTime: string;
+  estimatedArrivalTime: string;
   originAirportId: string;
   destinationAirportId: string;
+  mood: number;
+  tiredness: number;
+  love: number;
 }
+
+const dateTest = (date) => moment(date, "YYYY-MM-DD", true).isValid()
+const timeTest = (time) => moment(time, "HH:mm:ss", true).isValid()
 
 export const flightSchema = yup.object().shape({
   flightCode: yup.string().required(),
+  originAirportId: yup.string().required(),
+  destinationAirportId: yup.string().required(),
   date: yup
     .string()
     .test(
       "date-format",
       "date is not in the format YYYY-MM-DD, as required for DynamoDB filtering.",
-      (date) => moment(date, "YYYY-MM-DD", true).isValid()
+      dateTest
     )
     .required(),
-  originAirportId: yup.string().required(),
-  destinationAirportId: yup.string().required(),
+  estimatedDepartureTime: yup
+    .string()
+    .test(
+      "time-format",
+      "estimated departure time is not in the format HH:MM:SS",
+      timeTest
+    )
+    .required(),
+  estimatedArrivalTime: yup
+    .string()
+    .test(
+      "time-format",
+      "estimated arrival time is not in the format HH:MM:SS",
+      timeTest
+    )
+    .required(),
+  mood: yup.number().required(),
+  tiredness: yup.number().required(),
+  love: yup.number().required(),
 });
